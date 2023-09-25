8e2386c692961a5eed60bc77a2ceaa4966fd6c46
Brian-Pob <brianpoblete77777@gmail.com>
2023-09-24 21:13:46 -0400

    setup styles and packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import solidJs from '@astrojs/solid-js';
+import mdx from '@astrojs/mdx';
 
-import solidJs from "@astrojs/solid-js";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  site: 'https://astrokcd.vercel.app/',
+  integrations: [solidJs(), mdx(), sitemap()],
 });
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Brian Poblete';
+export const SITE_DESCRIPTION =
+  "Hi there! I'm a Tallahasse based UX Designer and Software Engineer. I'm passionately building and desiging for the web. Come get to know me!";
