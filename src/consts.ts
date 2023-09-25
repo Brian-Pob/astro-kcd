b52c4a08f58ba061db9b121399357a7248aac384
Brian-Pob <brianpoblete77777@gmail.com>
2023-09-24 21:29:32 -0400

    change consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Brian Poblete';
+export const SITE_TITLE = 'Astro KCD Template';
 export const SITE_DESCRIPTION =
-  "Hi there! I'm a Tallahasse based UX Designer and Software Engineer. I'm passionately building and desiging for the web. Come get to know me!";
+  "Astro project template based on Kent C Dodd's personal website.";
+export const SITE_NAME = 'Astro KCD';
